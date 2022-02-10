5767f09e63128f53f18b718574675dde75620416
MAbdurahman <mdbdrrhm@gmail.com>
2022-02-10 15:44:00 -0600

    PHPMailer operating proper

--- public/index.php
+++ public/index.php
@@ -14,6 +14,52 @@
     $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
     $dotenv->load();
 
+    /**
+     * Configure PHPMailer with your SMTP server settings
+     */
+    $mail = new PHPMailer();
 
-    echo "Welcome to php_send_email";
+    $mail->isSMTP();
+    $mail->Host = $_ENV['HOST'];
+    $mail->Port = $_ENV['PORT'];
+    $mail->SMTPAuth = true;
+    $mail->Username = $_ENV['USERNAME'];
+    $mail->Password = $_ENV['PASSWORD'];
+    $mail->SMTPSecure = 'tls';
+
+
+    /**
+     * Enable SMTP debug messages
+     */
+//    $mail->SMTPDebug = 2;
+
+
+    /**
+     * Send an email
+     */
+    $email = 'yrud1@hotmail.com';
+    $from = $_ENV['USERNAME'];
+    $subject = "Message from $from";
+    $message = 'This is a test!';
+
+    $mail->setFrom($_ENV['USERNAME']);
+    $mail->addAddress($email);
+    $mail->addReplyTo($_ENV['USERNAME']);
+
+
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+    $mail->Body = <<<EOT
+                    <h2 style="color: #333333">Email From $from</h2>
+                    <p>ReplyTo: &nbsp;<strong> $email; </strong></p>
+                    <p>Subject: &nbsp;<strong> $subject;</strong></p>
+                    <p>Message: &nbsp;<strong> $message; </strong></p>
+                    EOT;
+
+    if ($mail->send()) {
+        echo 'Message sent!';
+    } else {
+        echo 'Mailer error: ' . $mail->ErrorInfo;
+    }
+
 
