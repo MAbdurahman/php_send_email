982c976250248c88bf6ea2adee54fd2159b9bcb4
MAbdurahman <mdbdrrhm@gmail.com>
2022-02-10 15:49:50 -0600

    Add classes/Config for variables

---
+++ classes/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+    /**
+     * Config class
+     * PHP version 8.0.13
+     */
+    class Config
+    {
+        /**
+         * SMTP password
+         *
+         * @var string
+         */
+        const SHOW_ERRORS = true;
+
+    }//end of the Config class
